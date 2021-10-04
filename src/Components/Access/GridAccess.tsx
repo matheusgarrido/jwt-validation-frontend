2aa2fbeb7f493cad6bafe93f5d447a77f4fcb3bc
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-10-04 18:00:36 -0300

    Grid style class moved to another style file

--- src/Components/Access/GridAccess.tsx
+++ src/Components/Access/GridAccess.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Style from './Access.module.scss';
+import GridStyle from '../../assets/styles/GridStyle.module.scss';
 import { Card, Grid } from '@mui/material';
 
 function GridAccess(props: any) {
   return (
     <>
-      <Grid container className={Style.grid}>
+      <Grid container className={GridStyle.grid}>
         <Card className={Style.card} variant="outlined">
           <h1>{props.title}</h1>
           {props.children}
